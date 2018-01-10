aad753bfcdeaad397d6504b59950875a2582b60c
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-01-10 17:00:32 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,20 @@
+#include<Stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b||a>c)
+{
+printf("%d",a);
+}
+elseif(b>a||b>c)
+{
+printf("%d",b);
+}
+else
+{
+print("%d",c);
+}
+}
